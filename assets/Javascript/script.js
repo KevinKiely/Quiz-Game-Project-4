5ba35551b0f42d521232f3dfe0cebb32b5cc10ba
KevinKiely <Kevinkiely762@gmail.com>
2024-02-08 21:42:36 -0500

    Added timer, logic for event listeners and correct/incorrect answers, Game Over screen styling

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,42 +1,123 @@
 var navBar= document.querySelector("#navbar");
-var highScores= document.querySelector("#high-scores");
+var highScores= document.querySelector("#high-scores-button");
 var timer= document.querySelector("#timeLeft");
-var score= document.querySelector("#score");
+var gameScore= document.querySelector("#gameScore");
 var startMenu= document.querySelector("#start-menu");
 var startBtn= document.querySelector("#start-button");
 var app= document.querySelector(".app");
 var quiz= document.querySelector(".quiz");
-var correctBtn= document.querySelector("btn-correct");
-var incorrectBtn= document.querySelector("btn-incorrect");
+var answerOne= document.querySelector("#answer-1");
+var answerTwo= document.querySelector("#answer-2");
+var answerThree= document.querySelector("#answer-3");
+var answerFour= document.querySelector("#answer-4");
+var questionText= document.querySelector("#question-text");
+var gameOver= document.querySelector("#Game-Over");
+var finalScore= document.querySelector("#final-score");
+var playAgain= document.querySelector("#play-again");
 
 
 
+var questions= [
+    {
+        title: 'What is the capital of Afghanistan?',
+        options: ["Ottawa","Bogota","Kabul","Nassau"],
+        answer: "Kabul"
+    },
+    {
+        title: 'What is the capital of Peru?',
+        options: ["Lima","Bogota","Minsk","Berlin"],
+        answer: "Lima"
+    },
+    {
+        title: 'What is the capital of Hungary?',
+        options: ["New Delhi","Dublin","Jakarta","Budapest"],
+        answer: "Budapest"
+    },
+    {
+        title: 'What is the capital of Uganda?',
+        options: ["Bissau","Kampala","Tehran","Hanoi"],
+        answer: "Kampala"
+    },
+    {
+        title: 'What is the capital of Libya?',
+        options: ["Tripoli","Ottawa","Lusaka","Rome"],
+        answer: "Tripoli"
+    },
+];
 
 
 var score= 0;
 var timeLeft= 60;
 var questionNumber= 0;
 
-startBtn.addEventListener('click', startGame);
-correctBtn.addEventListener('click',correctAnswer);
-incorrectBtn.addEventListener('click', incorrectAnswer);
 
+startBtn.addEventListener('click', startGame);
+answerOne.addEventListener('click',checkAnswer);
+answerTwo.addEventListener('click',checkAnswer);
+answerThree.addEventListener('click',checkAnswer);
+answerFour.addEventListener('click',checkAnswer);
+
+
+function setQuestion() {
+questionText.textContent=questions[questionNumber].title;
+answerOne.textContent=questions[questionNumber].options[0];
+answerTwo.textContent=questions[questionNumber].options[1];
+answerThree.textContent=questions[questionNumber].options[2];
+answerFour.textContent= questions[questionNumber].options[3];
+}
+
+
+
+
+function startTimer() {
+    var timeInterval = setInterval (function () {
+        if (timeLeft >=1) {
+            timer.textContent = timeLeft;
+            timeLeft--;
+        } 
+        else {
+            timer.textContent = '';
+            clearInterval(timeInterval);
+            console.log("Game Over!");
+            app.setAttribute("style", "display:none");
+            quiz.setAttribute("style", "display:none");
+
+            gameOver.setAttribute("style","display:block");
+        }
+    }, 1000);
+}
 
 function startGame(event) {
-    console.log("Game Starting");
-    startMenu.setAttribute("style","display:none");
+    console.log("Game Starting;");
+    startTimer();
+    startMenu.setAttribute("style", "display:none");
     app.setAttribute("style", "display:block");
-    app[0].setAttribute("style", "display:block");
+    quiz.setAttribute("style","display:block");
+    setQuestion();
 }
 
 
-function correctAnswer(){
 
+function checkAnswer(event) {
+ var El=event.target;
+ var answer=questions[questionNumber].answer;
+ console.log("answer " + answer);
+ console.log(El.textContent);
+
+ if (El.textContent===answer) {
+score++;
+ gameScore.textContent = `Score: ${score}`;
+ questionNumber++;
+ setQuestion();
+ }
+ else {
+ console.log("Incorrect");
+ timeLeft=timeLeft-10;
+ questionNumber++;
+ setQuestion();
+ }
 }
 
-function incorrectAnswer() {
-
-}
 
 
 
@@ -44,36 +125,27 @@ function incorrectAnswer() {
 
 
 /*
-var start = document.getElementById('start-button');
-var welcome = document.querySelector('.welcome');
-var questionContainer = document.querySelector('.questions-container');
 
-// EVENT to trigger game start
-start.addEventListener('click', startGame);
+function checkAnswer(event){
+    var El=event.target;
+    var answer=questions[questionNumber].answer;
 
+    if(El.value===answer);
+    else();
+    
+    questionNumber++ ;
+    setQuestion();
 
-function startGame(event) {
-    console.log("Game Starting... ")
-    // hide WELCOME CONTAINER
-    welcome.classList.add('hide');
-    // unhide QUESTIONS CONTAINER
-    questionContainer.classList.remove('hide');
-
-    // we also want to start a TIMER 
 }
+
+
 */
 
 
 
 
 
-
-
-
-
 // Starting Page: Has the navbar, with a score, highscore (local storage), and timer elements. There is a large start button, with some explanation to the rules of the game below it// 
-
-
 
 
 //The start button will be connected to an event listener. Once the start button is clicked, the "quiz" element will become visible, the start button and explanation will become hidden/removed from the DOM. Also, a timer will start from 80 seconds. 
@@ -90,3 +162,24 @@ function startGame(event) {
 
 //Once the timer reaches 0 seconds or you finish all the questions, stop all activity however possible, and make a final screen appear with your score, a button to add to the HIGHSCORE list using a key.value pair, and a button to start the game again (using same function as the start button)
 
+
+/*
+
+var choice1 = document.getElementByID("choice1");
+choice.click = checkAnswer();
+
+var correctAnswer;
+
+
+//create html for title, 4 html elements for each option
+
+for (i=0; i<question.length();i++){
+//#1 Populate html element with correct data
+
+$(title).text =questions[i].title;
+$(options).text =questions[i].option;
+
+correctAnswer = questions[i].answer;
+}
+
+*/

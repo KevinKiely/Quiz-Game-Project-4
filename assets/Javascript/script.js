9f14c2c1edd674e6722a8f81639e97d78779b229
KevinKiely <Kevinkiely762@gmail.com>
2024-02-13 22:41:39 -0500

    Added high-score screen, a return to menu button, cleaned up code, added comments in javascript

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,7 +1,5 @@
 var navBar= document.querySelector("#navbar");
-var highScores= document.querySelector("#high-scores-button");
 var timer= document.querySelector("#timeLeft");
-var gameScore= document.querySelector("#gameScore");
 var startMenu= document.querySelector("#start-menu");
 var startBtn= document.querySelector("#start-button");
 var app= document.querySelector(".app");
@@ -12,58 +10,113 @@ var answerThree= document.querySelector("#answer-3");
 var answerFour= document.querySelector("#answer-4");
 var questionText= document.querySelector("#question-text");
 var gameOver= document.querySelector("#Game-Over");
-var finalScore= document.querySelector("#final-score");
-var playAgain= document.querySelector("#play-again");
+
+// This is for the Highscore Section
+var highScoresBtn= document.querySelector("#high-scores-button");
+var highScoresDisplay= document.querySelector("#high-scores");
+var goToMainMenuBtn= document.querySelector("#main-menu-button");
 
 
 
 var questions= [
     {
-        title: 'What is the capital of Afghanistan?',
+        title: '1. What is the capital of Afghanistan?',
         options: ["Ottawa","Bogota","Kabul","Nassau"],
         answer: "Kabul"
     },
     {
-        title: 'What is the capital of Peru?',
+        title: '2. What is the capital of Peru?',
         options: ["Lima","Bogota","Minsk","Berlin"],
         answer: "Lima"
     },
     {
-        title: 'What is the capital of Hungary?',
+        title: '3. What is the capital of Hungary?',
         options: ["New Delhi","Dublin","Jakarta","Budapest"],
         answer: "Budapest"
     },
     {
-        title: 'What is the capital of Uganda?',
+        title: '4. What is the capital of Uganda?',
         options: ["Bissau","Kampala","Tehran","Hanoi"],
         answer: "Kampala"
     },
     {
-        title: 'What is the capital of Libya?',
+        title: '5. What is the capital of Libya?',
         options: ["Tripoli","Ottawa","Lusaka","Rome"],
-        answer: "Tripoli"
+        answer: "Tripoli",
     },
 ];
 
 
+var finalScores= [
+    {
+        initials: '',
+        finalScore: '',
+}
+
+
+
+];
+
+//Universal Variables
 var score= 0;
 var timeLeft= 60;
 var questionNumber= 0;
+timer.textContent = '';
 
-
+// Starts the game
 startBtn.addEventListener('click', startGame);
-playAgain.addEventListener('click', startGame);
+//Event listeners for each answer button
 answerOne.addEventListener('click',checkAnswer);
 answerTwo.addEventListener('click',checkAnswer);
 answerThree.addEventListener('click',checkAnswer);
 answerFour.addEventListener('click',checkAnswer);
 
 
-function endGame() {
+// This will open the highscores page
+highScoresBtn.addEventListener('click',checkScores);
+
+function checkScores() {
+
+    app.setAttribute("style","display:none");
+    quiz.setAttribute("style", "display:none");
+    startMenu.setAttribute("style","display:none");
+    gameOver.setAttribute("style","display:none");
+    highScoresDisplay.setAttribute("style","display:block");
+
+
+    /*
+    highScoreOne.textContent= `${highScores[0].initials} = ${highScores[0].finalScore}`
+    */
+
 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Returns to the main menu from the HIGHSCORES table
+goToMainMenuBtn.addEventListener('click',goToMain);
+function goToMain() {
+    startMenu.setAttribute("style","display:block");
+    highScoresDisplay.setAttribute("style","display:none");
+    gameOver.setAttribute("style","display:none");
+
+}
+
+
+//Checks to see if there are any more questions, terminates the game if not//
 function setQuestion() {
 console.log(questions[questionNumber]);
 if (questions[questionNumber]===undefined) {
@@ -71,8 +124,8 @@ if (questions[questionNumber]===undefined) {
     app.setAttribute("style","display:none");
     quiz.setAttribute("style", "display:none");
     gameOver.setAttribute("style","display:block");
-    endGame();
-    
+    timeLeft=0;
+    console.log(score);
 } else {
 questionText.textContent=questions[questionNumber].title;
 answerOne.textContent=questions[questionNumber].options[0];
@@ -84,6 +137,8 @@ answerFour.textContent= questions[questionNumber].options[3];
 
 
 
+
+//Starts the timer
 function startTimer() {
     var timeInterval = setInterval (function () {
         if (timeLeft >=1) {
@@ -97,22 +152,23 @@ function startTimer() {
             app.setAttribute("style", "display:none");
             quiz.setAttribute("style", "display:none");
             gameOver.setAttribute("style","display:block");
-            endGame();
         }
     }, 1000);
 }
 
+//Starts the game
 function startGame(event) {
     console.log("Game Starting;");
     startTimer();
     startMenu.setAttribute("style", "display:none");
+    highScoresDisplay.setAttribute("style","display:none");
     app.setAttribute("style", "display:block");
     quiz.setAttribute("style","display:block");
     setQuestion();
 }
 
 
-
+//Checks to see if the answer button you clicked matches the answer value
 function checkAnswer(event) {
  var El=event.target;
  var answer=questions[questionNumber].answer;
@@ -132,69 +188,3 @@ score++;
  setQuestion();
  }
 }
-
-
-
-
-
-
-
-/*
-
-function checkAnswer(event){
-    var El=event.target;
-    var answer=questions[questionNumber].answer;
-
-    if(El.value===answer);
-    else();
-    
-    questionNumber++ ;
-    setQuestion();
-
-}
-
-
-*/
-
-
-
-
-
-// Starting Page: Has the navbar, with a score, highscore (local storage), and timer elements. There is a large start button, with some explanation to the rules of the game below it// 
-
-
-//The start button will be connected to an event listener. Once the start button is clicked, the "quiz" element will become visible, the start button and explanation will become hidden/removed from the DOM. Also, a timer will start from 80 seconds. 
-
-
-//Put all of your questions into an array "Question-1", "Question-2", etc*. Then use the .split command so separate them. 
-
-//Then, the first question wil become visible. It will have 4 answer buttons, each with an ID of correct or incorrect. There will be 2 more event listeners. Maybe use prevent default or stop propogation if things aren't working. The 2 event listeners are for either correct or incorrect responses.
-
-//For correct responses, increase the score count by 1, then make the current question invisible, increase the question in the array by 1, and add the visible attribute to the new question//
-
-//For incorrect questions, decrease the timer by 10 seconds, 
-
-
-//Once the timer reaches 0 seconds or you finish all the questions, stop all activity however possible, and make a final screen appear with your score, a button to add to the HIGHSCORE list using a key.value pair, and a button to start the game again (using same function as the start button)
-
-
-/*
-
-var choice1 = document.getElementByID("choice1");
-choice.click = checkAnswer();
-
-var correctAnswer;
-
-
-//create html for title, 4 html elements for each option
-
-for (i=0; i<question.length();i++){
-//#1 Populate html element with correct data
-
-$(title).text =questions[i].title;
-$(options).text =questions[i].option;
-
-correctAnswer = questions[i].answer;
-}
-
-*/
